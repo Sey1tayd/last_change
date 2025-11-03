17975fe38d3ab85522d3f629376a2ba23f07aa9b
Sey1tayd <ayd.seyit@gmail.com>
2025-11-03 10:03:14 +0300

    S3Y1T

--- sarac_ihsan/settings.py
+++ sarac_ihsan/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-this-in-production')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'Iamcurrentlywaching_howToGet1w1yw1th_Murder[2580]+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
